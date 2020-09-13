182f506e4f4ed9c79374524537274fb09fbff2a8
Odeilso Denis Leite <odeilson_2014@hotmail.com>
2020-09-12 21:51:46 -0300

    Iniciando projeto de API de Pacientes em ASP CORE Teste

---
+++ Controllers/PacienteController.cs
@@ -0,0 +1,27 @@
+using apiPacientes.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiPacientes.Controllers
+{
+    [Route("api/[Controller]")]
+    public class PacienteController : Controller
+    {
+        private readonly IPacienteRepository pacienteRepository;
+        public PacienteController(IPacienteRepository pacienteRepository)
+        {
+            this.pacienteRepository = pacienteRepository;
+        }
+
+        [HttpGet]
+        public string GetAll()
+        {
+            return "Olá";
+        }
+
+        [HttpGet("{id}", Name="GetPaciente")]
+        public string GetPacienteById(long id)
+        {
+            return "Paciente: " + id;
+        }
+    }
+}
